9ae59d45ccca8a9637deb9ab847d548e45f57fbb
hln5 <harrynguyen019@gmail.com>
2024-07-28 12:01:42 -0700

    ..

--- socket_server/tests/websocket_test.rs
+++ socket_server/tests/websocket_test.rs
@@ -7,9 +7,10 @@ mod tests {
     use socket_server::start_server;
     use tokio_tungstenite::{self, tungstenite};
     use tracing::level_filters::LevelFilter;
-    use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Layer};
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
 
-    const TEST_ADDR: &str = "127.0.0.1:5001";
+    const TEST_SERVER_ADDR: &str = "127.0.0.1:5001";
+    const TEST_REQUEST_HOST: &str = "localhost:3000";
 
     fn init_logging() {
         let stdout_log = tracing_subscriber::fmt::layer()
@@ -22,17 +23,17 @@ mod tests {
     async fn send_message() {
         init_logging();
 
-        tokio::spawn(start_server(TEST_ADDR)).into_future();
+        tokio::spawn(start_server(TEST_SERVER_ADDR)).into_future();
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
         let request = Request::builder()
-            .header("Origin", "http://localhost:3000")
-            .header("Host", "localhost:3000")
+            .header("Origin", format!("http://{TEST_REQUEST_HOST}"))
+            .header("Host", TEST_REQUEST_HOST)
             .header("Connection", "upgrade")
             .header("Upgrade", "websocket")
             .header("Sec-Websocket-Version", "13")
             .header("Sec-Websocket-Key", "dGhlIHNhbXBsZSBub25jZQ==")
-            .uri(format!("ws://{TEST_ADDR}/ws"))
+            .uri(format!("ws://{TEST_SERVER_ADDR}/ws"))
             .body(())
             .unwrap();
         let (mut send_socket, _response) = tokio_tungstenite::connect_async(request.clone())
@@ -48,17 +49,16 @@ mod tests {
             .await
             .expect("message sending should be successful");
 
-        let msg = recv_socket
+        let socket_msg = recv_socket
             .next()
             .await
             .expect("next item in stream should be present")
             .expect("item should be parasable");
 
-        let text = match msg {
+        let text = match socket_msg {
             tungstenite::Message::Text(text) => text,
             other => panic!("Expected a text message but got {other:?}"),
         };
-
-        assert_eq!(text, r#"{"username":"harry","text":"hello"}"#);
+        assert_eq!(text, json_str);
     }
 }
