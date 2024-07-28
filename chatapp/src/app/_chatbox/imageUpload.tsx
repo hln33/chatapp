c89ac7a5d373d10667d60f8985a8039d391c2940
hln5 <harrynguyen019@gmail.com>
2024-07-28 13:01:44 -0700

    Able to upload images to server

--- chatapp/src/app/_chatbox/imageUpload.tsx
+++ chatapp/src/app/_chatbox/imageUpload.tsx
@@ -3,10 +3,26 @@ import { ChangeEvent, useState } from 'react';
 export default function ImageUpload() {
   const [imageURL, setImageURL] = useState('');
 
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files === null) return;
 
     const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('username', 'test_user');
+    formData.append('image', file);
+
+    try {
+      const res = await fetch('http://localhost:3001/file_upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await res.text;
+      console.log(data);
+    } catch (err) {
+      console.error('Error uploading image', err);
+    }
+
     const imageURL = URL.createObjectURL(file);
     setImageURL(imageURL);
   };
--- socket_server/src/file_upload.rs
+++ socket_server/src/file_upload.rs
@@ -1,9 +1,23 @@
-use axum::{extract::Multipart, response::IntoResponse};
-use tracing::info;
+use axum::{extract::Multipart, http::StatusCode, response::IntoResponse};
+use tokio::io::AsyncWriteExt;
 
 pub async fn file_upload_handler(mut multipart: Multipart) -> impl IntoResponse {
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap();
-        info!("{name}");
+
+        if name == "image" {
+            let file_name = field.file_name().expect("image should have filename");
+            let file_path = format!("./public/uploads/{file_name}");
+            let data = field.bytes().await.expect("image bytes should be valid");
+
+            let mut file = tokio::fs::File::create(&file_path)
+                .await
+                .expect("file creation should be successful");
+            file.write_all(&data)
+                .await
+                .expect("file writing should be successful");
+        }
     }
+
+    (StatusCode::OK, "File uploaded successfully")
 }
