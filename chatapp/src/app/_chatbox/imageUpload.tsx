1db17893a4e50d56b3292df4c7f060048254098b
hln5 <harrynguyen019@gmail.com>
2024-07-28 13:33:56 -0700

    refactor image upload component

--- chatapp/src/app/_chatbox/imageUpload.tsx
+++ chatapp/src/app/_chatbox/imageUpload.tsx
@@ -1,4 +1,19 @@
 import { ChangeEvent, useState } from 'react';
+import Image from 'next/image';
+
+const uploadImage = async (formData: FormData) => {
+  try {
+    const res = await fetch('http://localhost:3001/file_upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await res.text();
+    console.log(data);
+  } catch (err) {
+    console.error('Error uploading image', err);
+  }
+};
 
 export default function ImageUpload() {
   const [imageURL, setImageURL] = useState('');
@@ -10,18 +25,7 @@ export default function ImageUpload() {
     const formData = new FormData();
     formData.append('username', 'test_user');
     formData.append('image', file);
-
-    try {
-      const res = await fetch('http://localhost:3001/file_upload', {
-        method: 'POST',
-        body: formData,
-      });
-
-      const data = await res.text();
-      console.log(data);
-    } catch (err) {
-      console.error('Error uploading image', err);
-    }
+    await uploadImage(formData);
 
     const imageURL = URL.createObjectURL(file);
     setImageURL(imageURL);
@@ -29,7 +33,15 @@ export default function ImageUpload() {
 
   return (
     <>
-      {imageURL && <img src={imageURL} />}
+      {imageURL && (
+        <Image
+          className="w-auto h-auto"
+          src={imageURL}
+          width={0}
+          height={0}
+          alt="preview"
+        />
+      )}
       <input type="file" onChange={handleImageChange} />
     </>
   );
