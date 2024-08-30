769bd2669412a618f1d0f66142384673eb5f5eb6
Harry Nguyen <harry.nguyen@motorolasolutions.com>
2024-08-30 13:14:49 -0700

    ..

--- chatapp/src/app/chat/_components/messageBubble.tsx
+++ chatapp/src/app/chat/_components/messageBubble.tsx
@@ -12,22 +12,20 @@ export default function MessageBubble({ message }: Props) {
     .map((line, index) => <p key={index}>{line}</p>);
 
   return (
-    <>
-      <div className={`chat chat-${fromCurrentUser ? 'start' : 'end'}`}>
-        {!fromCurrentUser && (
-          <div className="chat-header text-slate-400">{username}</div>
-        )}
-        <div
-          className={`chat-bubble ${
-            fromCurrentUser ? 'chat-bubble-primary' : ''
-          }`}
-        >
-          <span data-testid="message-text" className="h-2">
-            {text}
-          </span>
-          <ImagePreview imageURLs={image_urls} />
-        </div>
+    <div className={`chat chat-${fromCurrentUser ? 'start' : 'end'}`}>
+      {!fromCurrentUser && (
+        <div className="chat-header text-slate-400">{username}</div>
+      )}
+      <div
+        className={`chat-bubble ${
+          fromCurrentUser ? 'chat-bubble-primary' : ''
+        }`}
+      >
+        <span data-testid="message-text" className="h-2">
+          {text}
+        </span>
+        <ImagePreview imageURLs={image_urls} />
       </div>
-    </>
+    </div>
   );
 }
